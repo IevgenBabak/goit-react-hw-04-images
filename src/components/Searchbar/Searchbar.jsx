fa870d6b6061ebb7e2f02eae5ec18606ec36c01f
IevgenBabak <ievgenbabak@gmail.com>
2023-03-25 20:57:38 +0200

    upd r

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import { FaSearch } from 'react-icons/fa';
